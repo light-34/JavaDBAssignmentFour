39be91aeca5fb022f1cb762b358d6e78c8edae6e
Tim Burns <tim.marshall.burns95@gmail.com>
2021-04-04 17:34:44 -0400

    first commit

---
+++ src/Presentation/CustomerGUI.java
@@ -0,0 +1,221 @@
+package Presentation;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JTable;
+
+public class CustomerGUI extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtCust_ID;
+	private JTextField txtFName;
+	private JTextField txtLName;
+	private JTextField txtPhoneNo;
+	private JTextField txtEmail;
+	private JTextField txtStreet;
+	private JTextField txtCity;
+	private JTextField txtPostal;
+	private JTable tableCustomers;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CustomerGUI frame = new CustomerGUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public CustomerGUI() {
+		ProductGUI prod = new ProductGUI();
+		//CustomerGUI cust = new CustomerGUI();
+		setTitle("Customer Info");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 539, 622);
+		
+		JMenuBar menuCust = new JMenuBar();
+		setJMenuBar(menuCust);
+		
+		JMenu mnuSystem = new JMenu("System");
+		menuCust.add(mnuSystem);
+		
+		JMenuItem mnuItemProduct = new JMenuItem("Switch to Product");
+		mnuItemProduct.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				prod.setVisible(true);
+				//cust.dispose();
+			}
+		});
+		mnuSystem.add(mnuItemProduct);
+		
+		JMenuItem mnuItemExit = new JMenuItem("Exit");
+		mnuSystem.add(mnuItemExit);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblCust_ID = new JLabel("Customer ID");
+		lblCust_ID.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblCust_ID.setBounds(10, 23, 104, 17);
+		contentPane.add(lblCust_ID);
+		
+		JLabel lblFirst_Name = new JLabel("First Name");
+		lblFirst_Name.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblFirst_Name.setBounds(10, 51, 79, 17);
+		contentPane.add(lblFirst_Name);
+		
+		JLabel lblLast_Name = new JLabel("Last Name");
+		lblLast_Name.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblLast_Name.setBounds(229, 51, 79, 17);
+		contentPane.add(lblLast_Name);
+		
+		JLabel lblPhone_No = new JLabel("Phone#");
+		lblPhone_No.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblPhone_No.setBounds(10, 79, 79, 17);
+		contentPane.add(lblPhone_No);
+		
+		JLabel lblEmail = new JLabel("Email");
+		lblEmail.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblEmail.setBounds(229, 79, 79, 17);
+		contentPane.add(lblEmail);
+		
+		JLabel lblAutoGen = new JLabel("---> Will be auto generated for you");
+		lblAutoGen.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblAutoGen.setBounds(229, 23, 254, 17);
+		contentPane.add(lblAutoGen);
+		
+		JLabel lblCity = new JLabel("City");
+		lblCity.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblCity.setBounds(229, 107, 79, 17);
+		contentPane.add(lblCity);
+		
+		JLabel lblProvince = new JLabel("Province");
+		lblProvince.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblProvince.setBounds(10, 135, 79, 17);
+		contentPane.add(lblProvince);
+		
+		JLabel lblPostalCode = new JLabel("Postal Code");
+		lblPostalCode.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblPostalCode.setBounds(229, 135, 89, 17);
+		contentPane.add(lblPostalCode);
+		
+		txtCust_ID = new JTextField();
+		txtCust_ID.setBounds(111, 23, 120, 20);
+		contentPane.add(txtCust_ID);
+		txtCust_ID.setColumns(10);
+		
+		txtFName = new JTextField();
+		txtFName.setColumns(10);
+		txtFName.setBounds(99, 51, 120, 20);
+		contentPane.add(txtFName);
+		
+		txtLName = new JTextField();
+		txtLName.setColumns(10);
+		txtLName.setBounds(318, 51, 120, 20);
+		contentPane.add(txtLName);
+		
+		txtPhoneNo = new JTextField();
+		txtPhoneNo.setColumns(10);
+		txtPhoneNo.setBounds(99, 79, 120, 20);
+		contentPane.add(txtPhoneNo);
+		
+		txtEmail = new JTextField();
+		txtEmail.setColumns(10);
+		txtEmail.setBounds(318, 79, 120, 20);
+		contentPane.add(txtEmail);
+		
+		JLabel lblStreet = new JLabel("Street");
+		lblStreet.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblStreet.setBounds(10, 107, 79, 17);
+		contentPane.add(lblStreet);
+		
+		txtStreet = new JTextField();
+		txtStreet.setColumns(10);
+		txtStreet.setBounds(99, 107, 120, 20);
+		contentPane.add(txtStreet);
+		
+		txtCity = new JTextField();
+		txtCity.setColumns(10);
+		txtCity.setBounds(318, 107, 120, 20);
+		contentPane.add(txtCity);
+		
+		txtPostal = new JTextField();
+		txtPostal.setColumns(10);
+		txtPostal.setBounds(318, 135, 120, 20);
+		contentPane.add(txtPostal);
+		
+		JComboBox cmbBxProv = new JComboBox();
+		cmbBxProv.setModel(new DefaultComboBoxModel(new String[] {"Alberta", "British Columbia", "Manitoba", "New Brunswick", "Newfoundland ", "Nova Scotia", "Ontario", "PEI", "Quebec", "Saskatchewan", "NW Territories", "Nunavut", "Yukon"}));
+		cmbBxProv.setBounds(77, 134, 142, 22);
+		contentPane.add(cmbBxProv);
+		
+		JButton btnSave = new JButton("Save");
+		btnSave.setBounds(10, 162, 89, 23);
+		contentPane.add(btnSave);
+		
+		JButton btnDisplay = new JButton("Display");
+		btnDisplay.setBounds(142, 163, 89, 23);
+		contentPane.add(btnDisplay);
+		
+		JButton btnUpdate = new JButton("Update");
+		btnUpdate.setBounds(281, 163, 89, 23);
+		contentPane.add(btnUpdate);
+		
+		JButton btnExit = new JButton("Exit");
+		btnExit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				System.exit(0);
+			}
+		});
+		btnExit.setBounds(420, 162, 89, 23);
+		contentPane.add(btnExit);
+		
+		JButton btnFirst = new JButton("First");
+		btnFirst.setBounds(10, 535, 89, 23);
+		contentPane.add(btnFirst);
+		
+		JButton btnNext = new JButton("Next ");
+		btnNext.setBounds(142, 536, 89, 23);
+		contentPane.add(btnNext);
+		
+		JButton btnPrev = new JButton("Previous");
+		btnPrev.setBounds(281, 536, 89, 23);
+		contentPane.add(btnPrev);
+		
+		JButton btnLast = new JButton("Last");
+		btnLast.setBounds(420, 535, 89, 23);
+		contentPane.add(btnLast);
+		
+		tableCustomers = new JTable();
+		tableCustomers.setBounds(10, 209, 505, 300);
+		contentPane.add(tableCustomers);
+	}
+}
---
+++ src/Presentation/ProductGUI.java
@@ -0,0 +1,158 @@
+package Presentation;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JTable;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class ProductGUI extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtProduct_ID;
+	private JTextField txtProd_Name;
+	private JTextField txtPrice;
+	private JTable tableProducts;
+	private JTextField txtPriceRange1;
+	private JTextField txtPriceRange2;
+	private JTextField txtKeyword;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ProductGUI frame = new ProductGUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ProductGUI() {
+		setTitle("Product Info");		
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 496, 518);
+		
+		JMenuBar menuProd = new JMenuBar();
+		setJMenuBar(menuProd);
+		
+		JMenu mnuSystem = new JMenu("System");
+		menuProd.add(mnuSystem);
+		
+		JMenuItem MnuItemExit = new JMenuItem("Exit");
+		MnuItemExit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				System.exit(0);
+			}
+		});
+		mnuSystem.add(MnuItemExit);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblProd_ID = new JLabel("Product ID");
+		lblProd_ID.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblProd_ID.setBounds(10, 11, 104, 17);
+		contentPane.add(lblProd_ID);
+		
+		txtProduct_ID = new JTextField();
+		txtProduct_ID.setColumns(10);
+		txtProduct_ID.setBounds(111, 11, 120, 20);
+		contentPane.add(txtProduct_ID);
+		
+		JLabel lblAutoGen = new JLabel("---> Will be auto generated for you");
+		lblAutoGen.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblAutoGen.setBounds(228, 11, 254, 17);
+		contentPane.add(lblAutoGen);
+		
+		JLabel lblProd_Name = new JLabel("Product Name");
+		lblProd_Name.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblProd_Name.setBounds(10, 39, 104, 17);
+		contentPane.add(lblProd_Name);
+		
+		txtProd_Name = new JTextField();
+		txtProd_Name.setColumns(10);
+		txtProd_Name.setBounds(121, 39, 164, 20);
+		contentPane.add(txtProd_Name);
+		
+		JLabel lblPrice = new JLabel("List Price");
+		lblPrice.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblPrice.setBounds(305, 39, 71, 17);
+		contentPane.add(lblPrice);
+		
+		txtPrice = new JTextField();
+		txtPrice.setColumns(10);
+		txtPrice.setBounds(386, 39, 86, 20);
+		contentPane.add(txtPrice);
+		
+		tableProducts = new JTable();
+		tableProducts.setBounds(10, 67, 462, 302);
+		contentPane.add(tableProducts);
+		
+		JButton btnSave = new JButton("Save");
+		btnSave.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btnSave.setBounds(10, 416, 128, 37);
+		contentPane.add(btnSave);
+		
+		JButton btnFind = new JButton("Find");
+		btnFind.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btnFind.setBounds(177, 416, 128, 37);
+		contentPane.add(btnFind);
+		
+		JButton btnUpdate = new JButton("Update");
+		btnUpdate.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btnUpdate.setBounds(344, 416, 128, 37);
+		contentPane.add(btnUpdate);
+		
+		JLabel lblPriceRange = new JLabel("Price Range");
+		lblPriceRange.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblPriceRange.setBounds(10, 380, 86, 25);
+		contentPane.add(lblPriceRange);
+		
+		txtPriceRange1 = new JTextField();
+		txtPriceRange1.setBounds(106, 384, 32, 20);
+		contentPane.add(txtPriceRange1);
+		txtPriceRange1.setColumns(10);
+		
+		JLabel lblAnd = new JLabel("&");
+		lblAnd.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblAnd.setBounds(148, 383, 18, 18);
+		contentPane.add(lblAnd);
+		
+		txtPriceRange2 = new JTextField();
+		txtPriceRange2.setColumns(10);
+		txtPriceRange2.setBounds(166, 384, 32, 20);
+		contentPane.add(txtPriceRange2);
+		
+		JLabel lblKeyword = new JLabel("Keyword");
+		lblKeyword.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblKeyword.setBounds(208, 380, 86, 25);
+		contentPane.add(lblKeyword);
+		
+		txtKeyword = new JTextField();
+		txtKeyword.setColumns(10);
+		txtKeyword.setBounds(279, 384, 193, 20);
+		contentPane.add(txtKeyword);
+	}
+}
