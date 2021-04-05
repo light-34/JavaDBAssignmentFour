1bdd028257fb8fd953f98d5dcb7ee781db3f82d3
Tim Burns <tim.marshall.burns95@gmail.com>
2021-04-05 17:13:34 -0400

    created customer and product classes

---
+++ src/Business/Customers.java
@@ -0,0 +1,60 @@
+package Business;
+
+public class Customers {
+	private String fName, lName, phoneNo, email, street, city, province, postalCode;
+	private int custID;
+	public String getfName() {
+		return fName;
+	}
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+	public String getlName() {
+		return lName;
+	}
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getProvince() {
+		return province;
+	}
+	public void setProvince(String province) {
+		this.province = province;
+	}
+	public String getPostalCode() {
+		return postalCode;
+	}
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	public int getCustID() {
+		return custID;
+	}
+	public void setCustID(int custID) {
+		this.custID = custID;
+	}
+}
---
+++ src/Business/Products.java
@@ -0,0 +1,25 @@
+package Business;
+
+public class Products {
+	private int prodID;
+	private String productName;
+	private double listPrice;
+	public int getProdID() {
+		return prodID;
+	}
+	public void setProdID(int prodID) {
+		this.prodID = prodID;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public double getListPrice() {
+		return listPrice;
+	}
+	public void setListPrice(double listPrice) {
+		this.listPrice = listPrice;
+	}
+}
