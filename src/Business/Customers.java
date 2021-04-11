74da2b00d2873bb2d22f33e6e4f8273d1655158a
light-34 <71089214+light-34@users.noreply.github.com>
2021-04-11 17:58:22 -0400

    Add files via upload
    New Constructors have been added

--- src/Business/Customers.java
+++ src/Business/Customers.java
@@ -1,60 +1,115 @@
 package Business;
 
 public class Customers {
-	private String fName, lName, phoneNo, email, street, city, province, postalCode;
-	private int custID;
+	private int customerid;
+	private String fName;
+	private String lName;
+	private String phoneNo;
+	private String email;
+	private String street;
+	private String city;
+	private Object province;
+	private String postalCode;
+	
+	
+	
+	public Customers(int customerid, String fName, String lName, String phoneNo, String email, String street,
+			String city, Object province, String postalCode) {
+		this.customerid = customerid;
+		this.fName = fName;
+		this.lName = lName;
+		this.phoneNo = phoneNo;
+		this.email = email;
+		this.street = street;
+		this.city = city;
+		this.province = province;
+		this.postalCode = postalCode;
+	}
+
+	public Customers(String fName, String lName, String phoneNo, String email, String street, String city,
+			Object province, String postalCode) {
+		this.fName = fName;
+		this.lName = lName;
+		this.phoneNo = phoneNo;
+		this.email = email;
+		this.street = street;
+		this.city = city;
+		this.province = province;
+		this.postalCode = postalCode;
+	}
+
+	public int getCustomerid() {
+		return customerid;
+	}
+
+	public void setCustomerid(int customerid) {
+		this.customerid = customerid;
+	}
+
 	public String getfName() {
 		return fName;
 	}
+
 	public void setfName(String fName) {
 		this.fName = fName;
 	}
+
 	public String getlName() {
 		return lName;
 	}
+
 	public void setlName(String lName) {
 		this.lName = lName;
 	}
+
 	public String getPhoneNo() {
 		return phoneNo;
 	}
+
 	public void setPhoneNo(String phoneNo) {
 		this.phoneNo = phoneNo;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getStreet() {
 		return street;
 	}
+
 	public void setStreet(String street) {
 		this.street = street;
 	}
+
 	public String getCity() {
 		return city;
 	}
+
 	public void setCity(String city) {
 		this.city = city;
 	}
-	public String getProvince() {
+
+	public Object getProvince() {
 		return province;
 	}
+
 	public void setProvince(String province) {
 		this.province = province;
 	}
+
 	public String getPostalCode() {
 		return postalCode;
 	}
+
 	public void setPostalCode(String postalCode) {
 		this.postalCode = postalCode;
 	}
-	public int getCustID() {
-		return custID;
-	}
-	public void setCustID(int custID) {
-		this.custID = custID;
-	}
+	
+	
+	
 }
--- src/Business/Products.java
+++ src/Business/Products.java
@@ -1,25 +1,46 @@
 package Business;
 
 public class Products {
-	private int prodID;
+	private int productid;
 	private String productName;
 	private double listPrice;
-	public int getProdID() {
-		return prodID;
+	
+	
+	
+	public Products(int productid, String productName, double listPrice) {
+		super();
+		this.productid = productid;
+		this.productName = productName;
+		this.listPrice = listPrice;
 	}
-	public void setProdID(int prodID) {
-		this.prodID = prodID;
+
+	public Products(String productName, double listPrice) {
+		this.productName = productName;
+		this.listPrice = listPrice;
 	}
+	
+	public int getProductid() {
+		return productid;
+	}
+
+	public void setProductid(int productid) {
+		this.productid = productid;
+	}
+
 	public String getProductName() {
 		return productName;
 	}
+
 	public void setProductName(String productName) {
 		this.productName = productName;
 	}
+
 	public double getListPrice() {
 		return listPrice;
 	}
+
 	public void setListPrice(double listPrice) {
 		this.listPrice = listPrice;
 	}
+
 }
